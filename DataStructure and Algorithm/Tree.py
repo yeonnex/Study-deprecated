be2c891b364090ee354f92884fd9a325dc0a7c16
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-01 22:39:58 +0900

    스프링 프로젝트 만들기

---
+++ DataStructure and Algorithm/Tree.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node6 = TreeNode(6)
+node7 = TreeNode(7)
+
+node1.left = node2
+node1.right = node3
+node2.left = node4
+node2.right = node5
+node3.left = node6
+node3.right = node7
+
+
+# preOrder NLR
+def recursivePreOrder(node):
+    if node is None:
+        return
+    print(node.val, end=" ")
+    recursivePreOrder(node.left)
+    recursivePreOrder(node.right)
+
+recursivePreOrder(node1)
